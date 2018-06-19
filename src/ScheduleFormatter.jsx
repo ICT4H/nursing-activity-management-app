bf31b417ef4a502684a2858a861947d6c70df0f7
yogiraj99 <ytambake@gmail.com>
2018-06-19 13:58:28 +0530

    Instead of hardcoded symbols for schedule status getting symbol hex codes for each status from constant.js to give flexibility to change symbols

--- src/ScheduleFormatter.jsx
+++ src/ScheduleFormatter.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {ADMINISTRATED, NOTADMINISTRATED, TOBEADMINISTRATED} from "./constants";
+import {SYMBOLSHEXCODE} from "./constants";
 
 function FormatTime(time) {
   //AM and PM not showing currently (24 hours clock);
@@ -8,24 +8,15 @@ function FormatTime(time) {
 
 class ScheduleFormatter extends Component {
   render() {
-    let schedule=this.props.schedule;
+    let schedule = this.props.schedule;
     return (<div className="schedule">
       <span>{FormatTime(schedule.scheduledTime)} {this.getScheduleStatus()}</span>
     </div>)
   }
 
-  //3 ifs looks very bad
   getScheduleStatus() {
-    if(this.props.schedule.status===ADMINISTRATED){
-      return <span>&#x2705;</span>;
-    }
-    if(this.props.schedule.status===TOBEADMINISTRATED){
-      return <span>&#x23F0;</span>
-      // return <p>&#x23F1;</p>;
-    }
-    if(this.props.schedule.status===NOTADMINISTRATED){
-            return <span>&#x26A0;</span>
-    }
+    let statusHexCode = SYMBOLSHEXCODE[this.props.schedule.status];
+    return <span className="scheduleStatus">{String.fromCharCode(statusHexCode)}</span>;
   }
 }
 
--- test/ScheduleFormatter.test.js
+++ test/ScheduleFormatter.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ScheduleFormatter from '../src/ScheduleFormatter';
 import {mount} from "enzyme";
+import {ADMINISTRATED, NOTADMINISTRATED, SYMBOLSHEXCODE, TOBEADMINISTRATED} from "../src/constants";
 
 test('Should have time of given schedule', () => {
   let schedule = {scheduledTime:new Date("June 8, 2018 2:30:00")};
@@ -10,5 +11,29 @@ test('Should have time of given schedule', () => {
   expect(component.findWhere(element=>element.hasClass("scheduleTime")).text()).toBe("2:30");
 });
 
+test('Should have symbol of clock when schedule status is to be administered', () => {
+  let schedule = {scheduledTime:new Date("June 8, 2018 2:30:00"),status:TOBEADMINISTRATED};
+  const component = mount(
+      <ScheduleFormatter schedule={schedule}/>,
+  );
+  const clockSymbol = String.fromCharCode(SYMBOLSHEXCODE.TOBEADMINISTRATED);
+  expect(component.findWhere(element=>element.hasClass("scheduleStatus")).text()).toBe(clockSymbol);
+});
 
-//Need to write test for getting emoji based on schedule status
+test('Should have symbol of danger when schedule status is not administrated', () => {
+  let schedule = {scheduledTime:new Date("June 8, 2018 2:30:00"),status:NOTADMINISTRATED};
+  const component = mount(
+      <ScheduleFormatter schedule={schedule}/>,
+  );
+  const clockSymbol = String.fromCharCode(SYMBOLSHEXCODE.NOTADMINISTRATED);
+  expect(component.findWhere(element=>element.hasClass("scheduleStatus")).text()).toBe(clockSymbol);
+});
+
+test('Should have symbol of checkMark when schedule status is to be administered', () => {
+  let schedule = {scheduledTime:new Date("June 8, 2018 2:30:00"),status:ADMINISTRATED};
+  const component = mount(
+      <ScheduleFormatter schedule={schedule}/>,
+  );
+  const clockSymbol = String.fromCharCode(SYMBOLSHEXCODE.ADMINISTRATED);
+  expect(component.findWhere(element=>element.hasClass("scheduleStatus")).text()).toBe(clockSymbol);
+});
